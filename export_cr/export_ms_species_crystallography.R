e4793408b5c47154792595669eac5658d685e522
liubomyrgavryliv <liubomyr.gavryliv@gmail.com>
2020-12-20 17:42:51 +0100

    added validation of ns_space_group_name and space_group_name

--- export_cr/export_ms_species_crystallography.R
+++ export_cr/export_ms_species_crystallography.R
@@ -9,7 +9,7 @@ path <- '../export/'
 initial <- googlesheets4::read_sheet(
   ss='1QA-Y229WNurpJA7KYmpiU2jn-_YJmrUfLq_lv0VFpXg',
   sheet = 'Crystallography',
-  range = 'A:O',
+  range = 'A:O',col_types = 'c',
   col_names = TRUE,
 )
 
@@ -25,18 +25,35 @@ crystal_systems_list <- tbl(conn, 'crystal_systems_list')
 crystal_classes_list <- tbl(conn, 'crystal_classes_list')
 ms_species <- tbl(conn, 'ms_species')
 # PROCESS data -----------------------------------------------------------------
-ms_species_crystal <-
-  initial %>%
+# validate crystal_system_name
+validate <- initial %>%
+  select(`Crystal System`) %>%
+  distinct(`Crystal System`) %>%
+  left_join(crystal_systems_list,by = c('Crystal System' = 'crystal_system_name'), copy=TRUE) %>%
+  filter(!is.na('Crystal System') && is.na(crystal_system_id)) # check if all crystal systems are present
+
+# validate crystal_class_name
+validate <- initial %>%
   select(`Class Name`) %>%
   mutate(crystal_class_name=str_split(`Class Name`, ',|;|, ')) %>%
   unchop(crystal_class_name, keep_empty = TRUE) %>%
   distinct(crystal_class_name) %>%
   arrange(crystal_class_name) %>%
   left_join(crystal_classes_list,by = 'crystal_class_name', copy=TRUE) %>%
-  filter(is.na('Crystal System') & is.na(crystal_system_id)) # check if all crystal classes are present
+  filter(!is.na('Crystal Class') && is.na(crystal_class_id)) # check if all crystal classes are present
 
-  # left_join(crystal_systems_list,by = c('Crystal System' = 'crystal_system_name'), copy=TRUE) %>%
-  # filter(is.na('Crystal System') & is.na(crystal_system_id)) # check if all crystal systems are present
+# validate space_group_name and ns_space_group_name
+validate <- initial %>%
+  select(Mineral_Name,`Space group`, Note) %>%
+  filter(str_detect(Note, 'standard')) %>%
+  mutate(ns_space_group_name=str_split(`Space group`, ', |;|,')) %>%
+  unchop(ns_space_group_name, keep_empty = TRUE) %>%
+  left_join(space_groups_list, by=c('ns_space_group_name'='space_group_name'), copy=TRUE) %>%
+  filter(is.na(space_group_id)) %>%
+  left_join(ns_space_groups_list, by='ns_space_group_name', copy=TRUE) %>%
+  filter(is.na(ns_space_group_id))
+  
+
   
 
 # LOAD into DB

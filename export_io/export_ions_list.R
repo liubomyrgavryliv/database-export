7644164903b7e8025ecc7ac6aadfc8a80214c08e
liubomyrgavryliv <liubomyr.gavryliv@gmail.com>
2020-12-26 14:15:24 +0100

    upload ion_list from google sheets

--- export_io/export_ions_list.R
+++ export_io/export_ions_list.R
@@ -22,107 +22,14 @@ ion_subgroup_list = tbl(conn, 'ion_subgroup_list')
 ion_type_list = tbl(conn, 'ion_type_list')
 
 #Load data ---------------------------------------------------------------------
-cations <- googlesheets4::read_sheet(
+initial <- googlesheets4::read_sheet(
   ss='1FCu3ywv1wVMP6IZjwFezjipXXC74S8hRbLZaNzZCWpg',
-  sheet = 'Cations',
-  range = 'A:O',
+  sheet = 'Ions_data',
+  range = 'A:U',
   col_names = TRUE,
   col_types = 'c',
   na = ""
 ) 
-
-anions <- googlesheets4::read_sheet(
-  ss='1FCu3ywv1wVMP6IZjwFezjipXXC74S8hRbLZaNzZCWpg',
-  sheet = 'Anions',
-  range = 'A:O',
-  col_names = TRUE,
-  col_types = 'c',
-  na = ""
-) 
-
-other <- googlesheets4::read_sheet(
-  ss='1FCu3ywv1wVMP6IZjwFezjipXXC74S8hRbLZaNzZCWpg',
-  sheet = 'Neutral, organic and other compounds',
-  range = 'A:M',
-  col_names = TRUE,
-  col_types = 'c',
-  na = ""
-) 
-
-
-silicates <- googlesheets4::read_sheet(
-  ss='1FCu3ywv1wVMP6IZjwFezjipXXC74S8hRbLZaNzZCWpg',
-  sheet = 'Silicates',
-  range = 'A:N',
-  col_names = TRUE,
-  col_types = 'c',
-  na = ""
-) 
-
-# Parse and merge into one table
-anions <- anions %>%
-  left_join(io_class, by=c('Class'='class_name'), copy=T) %>%
-  left_join(io_subclass, by=c('Subclass'='subclass_name'), copy=T) %>%
-  left_join(io_group, by=c('Group'='group_name'), copy=T) %>%
-  left_join(io_subgroup, by=c('Subgroup'='subgroup_name'), copy=T) %>%
-  mutate(expressed_as = NA_character_) %>%
-  select(`Name (short name)`, Ion, `Ion (oxidation states)`, `Variety of`, expressed_as, class_id, subclass_id, group_id,
-         subgroup_id, `Structure description`, Geometry, Type) %>%
-  rename(ion_name=`Name (short name)`, formula=Ion, formula_with_oxidation=`Ion (oxidation states)`,
-         variety_of=`Variety of`, structure_description=`Structure description`,
-         geometry=Geometry, ion_type_name=Type)
-
-cations <- cations %>%
-  left_join(io_class, by=c('Class'='class_name'), copy=T) %>%
-  left_join(io_subclass, by=c('Subclass'='subclass_name'), copy=T) %>%
-  mutate(variety_of = NA_character_,
-         group_id=NA_character_,
-         subgroup_id=NA_character_) %>%
-  select(`Name (long name)`, Ion, `Ion (oxidation states)`,variety_of, `Can be expressed as`, class_id, subclass_id, group_id, 
-         subgroup_id, `Structure description`, Geometry, Type) %>%
-  rename(ion_name=`Name (long name)`, formula=Ion, formula_with_oxidation=`Ion (oxidation states)`,expressed_as=`Can be expressed as`,
-         structure_description=`Structure description`,
-         geometry=Geometry, ion_type_name=Type)
-
-silicates <- silicates %>%
-  left_join(io_class, by=c('Class'='class_name'), copy=T) %>%
-  left_join(io_subclass, by=c('Subclass'='subclass_name'), copy=T) %>%
-  left_join(io_subgroup, by=c('Subgroup'='subgroup_name'), copy=T) %>%
-  mutate(group_id=NA_character_, expressed_as=NA_character_, geometry=NA_character_,
-         ion_type_name='Silicate') %>%                                                 # OMIT THIS AFTER RESOLVE ----------------------------------------
-  select(`Name (short name)`, Ion, `Ion (oxidation states)`, `Variety of`, expressed_as, class_id, subclass_id, group_id, 
-         subgroup_id, `Name (long name)`, geometry, ion_type_name) %>%
-  rename(ion_name=`Name (short name)`, formula=Ion, formula_with_oxidation=`Ion (oxidation states)`, variety_of=`Variety of`,
-         structure_description=`Name (long name)`)
-
-other <- other %>%
-  left_join(io_class, by=c('Class'='class_name'), copy=T) %>%
-  left_join(io_subclass, by=c('Subclass'='subclass_name'), copy=T) %>%
-  mutate(group_id=NA_character_,subgroup_id=NA_character_, expressed_as=NA_character_, variety_of=NA_character_) %>%
-  select(`Name (long name)`, Ion, `Ion (oxidation states)`, variety_of, expressed_as, class_id, subclass_id, group_id, 
-       subgroup_id, `Structure description`, Geometry, Type) %>%
-  rename(ion_name=`Name (long name)`, formula=Ion, formula_with_oxidation=`Ion (oxidation states)`,
-         structure_description=`Structure description`, geometry=Geometry, ion_type_name=Type)
-
-ions = rbind(cations, anions, silicates, other) %>%
-  arrange(ion_type_name, ion_name) %>%
-  left_join(io_types_list, by=c('ion_type_name'='ion_type_name'), copy=TRUE) %>%
-  filter(!is.na(ion_name)) %>% # OMIT AFTER RESOLVE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  distinct(ion_type_id, formula, .keep_all = TRUE) %>% # OMIT AFTER VITALII REMOVE DUPLICATE SILICATE !!!!!!!!!!!!!!!!!!!!
-  mutate(ion_id=row_number()) %>%
-  select(c('ion_id',"ion_type_id","ion_name","formula","formula_with_oxidation","variety_of",
-           "expressed_as","class_id","subclass_id","group_id","subgroup_id","structure_description",
-           "geometry") )
-
-ions_ids <- ions %>%
-  select(formula) %>%
-  mutate(variety_id = row_number())
-  
-ions_list <- ions %>%
-  left_join(ions_ids, by=c('variety_of'='formula')) %>%
-  mutate(variety_of=variety_id) %>%
-  select(!variety_id)
-  
 
 # Parse anions ----------------------------------------------------------------
 

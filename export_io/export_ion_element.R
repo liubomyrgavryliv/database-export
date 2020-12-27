0d7f6b187a3874fb0520d9e44286da055c6a09cc
liubomyrgavryliv <liubomyr.gavryliv@gmail.com>
2020-12-27 16:39:57 +0100

    export ion_element to db

--- export_io/export_ion_element.R
+++ export_io/export_ion_element.R
@@ -15,51 +15,39 @@ conn <- dbConnect(RPostgres::Postgres(),dbname = 'postgres',
                   password = 'BQBANe++XrmO5xWA3UqipNACx3Mf95kN')
 
 
-ion_class_list = tbl(conn, 'ion_class_list')
-ion_subclass_list = tbl(conn, 'ion_subclass_list')
-ion_group_list = tbl(conn, 'ion_group_list')
-ion_subgroup_list = tbl(conn, 'ion_subgroup_list')
-ion_type_list = tbl(conn, 'ion_type_list')
+ion_list = tbl(conn, 'ion_list')
+element_list = tbl(conn, 'element_list')
 
 #Load data ---------------------------------------------------------------------
 initial <- googlesheets4::read_sheet(
   ss='1FCu3ywv1wVMP6IZjwFezjipXXC74S8hRbLZaNzZCWpg',
   sheet = 'Ions_data',
-  range = 'A:X',
+  range = 'A:H',
   col_names = TRUE,
   col_types = 'c',
   na = ""
 ) 
 
 # Parse ions ----------------------------------------------------------------
+ion_list_local <- 
+  ion_list %>% 
+            collect()
 
-variety_of_check <- initial %>%
-  select(variety_of) %>%
-  distinct(variety_of) %>%
-  filter(!is.na(variety_of)) %>%
-  left_join(initial, by=c('variety_of' = 'formula'))
+ion_element <- ion_list_local %>%
+  select(ion_id, formula) %>%
+  mutate(element=str_match_all(formula, '[A-Z][a-z]?')) %>%
+  mutate(element=ifelse(formula=='REE', list(c('Ce', 'Dy', 'Er', 'Eu', 'Gd', 'Ho', 'La', 'Lu', 'Nd', 'Pr', 'Pm', 'Sm', 'Sc', 'Tb', 'Tm', 'Yb', 'Y')), element)) %>%
+  mutate(element=ifelse(formula=='Ln^3+^', list(c('Ce', 'Dy', 'Er', 'Gd', 'La', 'Nd', 'Pr', 'Sm', 'Yb')), element)) %>%
+  unchop(element, keep_empty = TRUE) %>%
+  distinct(ion_id, element) %>%
+  left_join(element_list, by='element', copy=TRUE) %>%
+  select(ion_id, element_id)
 
-initial_ions <- initial %>%
-  select(formula) %>%
-  mutate(ion_id=row_number())
-
-ion_list <- initial %>%
-  select(ion_type_name, ion_name, formula, formula_with_oxidation, overall_charge, variety_of, expressed_as, element_or_sulfide,
-         ion_class_name, ion_subclass_name, ion_group_name, ion_subgroup_name, structure_description, geometry) %>%
-  left_join(ion_type_list, by='ion_type_name', copy=TRUE) %>%
-  left_join(ion_class_list, by='ion_class_name', copy=TRUE) %>%
-  left_join(ion_subclass_list, by='ion_subclass_name', copy=TRUE) %>%
-  left_join(ion_group_list, by='ion_group_name', copy=TRUE) %>%
-  left_join(ion_subgroup_list, by='ion_subgroup_name', copy=TRUE) %>%
-  left_join(initial_ions, by=c('variety_of'='formula'), copy=TRUE) %>%
-  select(ion_type_id, ion_name, formula, formula_with_oxidation, overall_charge, ion_id, expressed_as, element_or_sulfide, ion_class_id, ion_subclass_id,
-         ion_group_id, ion_subgroup_id, structure_description, geometry) %>%
-  rename(variety_of = ion_id)
 
 
 # UPLOAD DATA TO DB
-dbSendQuery(conn, "TRUNCATE TABLE ion_list RESTART IDENTITY CASCADE;")
-dbWriteTable(conn, "ion_list", ion_list, append=TRUE)
+dbSendQuery(conn, "TRUNCATE TABLE ion_element RESTART IDENTITY CASCADE;")
+dbWriteTable(conn, "ion_element", ion_element, append=TRUE)
 
 # Disconnect from the DB
 dbDisconnect(conn)

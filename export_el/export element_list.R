631374569ce966f20383c508e6f9d65388cef111
liubomyrgavryliv <liubomyr.gavryliv@gmail.com>
2020-12-28 19:10:38 +0100

    update element_list

--- export_el/export element_list.R
+++ export_el/export element_list.R
@@ -13,6 +13,10 @@ conn <- dbConnect(RPostgres::Postgres(),dbname = 'postgres',
                   user = 'postgres',
                   password = 'BQBANe++XrmO5xWA3UqipNACx3Mf95kN')
 
+goldschmidt_class_list = tbl(conn, 'goldschmidt_class_list')
+bonding_type_list = tbl(conn, 'bonding_type_list')
+chemical_group_list = tbl(conn, 'chemical_group_list')
+phase_state_list = tbl(conn, 'phase_state_list')
 
 #Load data ---------------------------------------------------------------------
 initial <- googlesheets4::read_sheet(
@@ -25,7 +29,11 @@ initial <- googlesheets4::read_sheet(
 ) 
 
 # PROCESS DATA -----------------------------------------------------------------
-el_data <- initial %>%
+element_list <- initial %>%
+  left_join(goldschmidt_class_list, by=c('goldschmidt_classification'='goldschmidt_class_name'), copy=TRUE) %>%
+  left_join(bonding_type_list, by=c('bonding_type'='bonding_type_name'), copy=TRUE) %>%
+  left_join(chemical_group_list, by=c('group_block'='chemical_group_name'), copy=TRUE) %>%
+  left_join(phase_state_list, by=c('standard_state'='phase_state_name'), copy=TRUE) %>%
   mutate(atomic_mass = str_replace(atomic_mass, ',', '.'),
          electronegativity = str_replace(electronegativity, ',', '.'),
          ion_radius = str_replace(ion_radius, ',', '.'),
@@ -45,7 +53,18 @@ el_data <- initial %>%
          solar_system_ahrens = str_replace(solar_system_ahrens, ',', '.'),
          solar_system_ahrens_with_uncertainty = str_replace(solar_system_ahrens_with_uncertainty, ',', '.')
   ) %>%
-  arrange(!desc(atomic_number))
+  arrange(!desc(atomic_number)) %>%
+  select(element,name,atomic_number,name_alternative,atomic_mass,atomic_mass_standard_uncertainty,electronic_configuration,cpk_hex_color,                       
+         goldschmidt_class_id,electronegativity,empirical_atomic_radius,calculated_atomic_radius,            
+         van_der_waals_radius,covalent_single_bond_atomic_radius,covalent_triple_bond_atomic_radius,metallic_atomic_radius,              
+         ion_radius,ion_radius_charge,ionization_energy,electron_affinity,                   
+         oxidation_states,phase_state_id,bonding_type_id,melting_point,                       
+         boiling_point,density,chemical_group_id,crust_crc_handbook,                  
+         crust_kaye_laby,crust_greenwood,crust_ahrens_taylor,crust_ahrens_wanke,                  
+         crust_ahrens_waver,upper_crust_ahrens_taylor,upper_crust_ahrens_shaw,sea_water_crc_handbook,              
+         sea_water_kaye_laby,sun_kaye_laby,solar_system_kaye_laby,solar_system_ahrens,                 
+         solar_system_ahrens_with_uncertainty,natural_isotopes,name_meaning,discovery_year,                      
+         discoverer,application,safety,biological_role)
 
 # UPLOAD DATA TO DB
 dbSendQuery(conn, "TRUNCATE TABLE element_list RESTART IDENTITY CASCADE;")

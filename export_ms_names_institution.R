c2ac7464ea25f1d6930d386b82568656693ef0de
liubomyrgavryliv <liubomyr.gavryliv@gmail.com>
2020-12-29 20:02:09 +0100

    updated mineral_name_locality

--- export_ms_names_institution.R
+++ export_ms_names_institution.R
@@ -8,12 +8,11 @@ rm(list=ls())
 path <- 'export/'
 source('functions.R')
 
-conn <- dbConnect(RPostgres::Postgres(),dbname = 'master', 
-                  host = 'ec2-18-184-252-245.eu-central-1.compute.amazonaws.com',
-                  port = 5433,
+conn <- dbConnect(RPostgres::Postgres(),dbname = 'postgres', 
+                  host = 'master.c6ya4cff5frj.eu-central-1.rds.amazonaws.com',
+                  port = 5432,
                   user = 'postgres',
                   password = 'BQBANe++XrmO5xWA3UqipNACx3Mf95kN')
-
 
 #Load data ---------------------------------------------------------------------
 initial <- googlesheets4::read_sheet(
@@ -25,31 +24,30 @@ initial <- googlesheets4::read_sheet(
   na = ""
 ) 
 
-countries_list <- tbl(conn, 'countries_list')
-ms_species = tbl(conn, 'ms_species')
+country_list <- tbl(conn, 'country_list')
+mineral_list = tbl(conn, 'mineral_list')
 # PROCESS DATA -----------------------------------------------------------------
-ms_names_institution <- initial %>%
+mineral_name_institution <- initial %>%
   filter(!is.na(`GROUP/INSTITUTION`)) %>%
   select(Mineral_Name, `GROUP/INSTITUTION`, About, Nationality__1)
 
 # COMPARE WITH languages_list --------- OMIT THIS STEP !!!!! -------------------
 countries_bugs <- ms_names_institution %>%
-  anti_join(countries_list, by = c('Nationality__1' = 'name'), copy=TRUE)
+  anti_join(country_list, by = c('Nationality__1' = 'country_name'), copy=TRUE)
 
 # replace countries names with ids
-ms_names_institution <-
-  ms_names_institution %>%
-  left_join(countries_list, by=c('Nationality__1' = 'name'), copy=TRUE) %>%
-  select(Mineral_Name, `GROUP/INSTITUTION`, About, id) %>%
-  rename(name=Mineral_Name, institution_name = `GROUP/INSTITUTION`, note=About, country_id=id) %>%
-  left_join(ms_species, by=c('name'='mineral_name'), copy=TRUE) %>%
+mineral_name_institution <-
+  mineral_name_institution %>%
+  left_join(country_list, by=c('Nationality__1' = 'country_name'), copy=TRUE) %>%
+  select(Mineral_Name, `GROUP/INSTITUTION`, About, country_id) %>%
+  rename(name=Mineral_Name, institution_name = `GROUP/INSTITUTION`, note=About) %>%
+  left_join(mineral_list, by=c('name'='mineral_name'), copy=TRUE) %>%
   rename(note=note.x) %>%
   select(mineral_id, institution_name, note, country_id)
 
 # UPLOAD DATA TO DB
-dbSendQuery(conn, "DELETE FROM ms_names_institution;")
-dbWriteTable(conn, "ms_names_institution", ms_names_institution, append=TRUE)
-dbDisconnect(conn)
+dbSendQuery(conn, "TRUNCATE TABLE mineral_name_institution RESTART IDENTITY CASCADE;")
+dbWriteTable(conn, "mineral_name_institution", mineral_name_institution, append=TRUE)
 
-# EXPORT ms_species ------------------------------------------------------------
-write_csv(ms_names_institution, path = paste0(path, 'ms_names_institution.csv'), na='')
+
+dbDisconnect(conn)
